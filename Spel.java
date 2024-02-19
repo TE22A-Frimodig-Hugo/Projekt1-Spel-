3a5b553e82abdd8f2b9909b833a15f4869be5e15
Hugo Frimodig <Hugo.Frimodig@elev.ga.ntig.se>
2024-02-19 08:55:09 +0100

    klart

--- Spel.java
+++ Spel.java
@@ -1,62 +1,66 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Spel{
+public class Spel {
 
-
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws InterruptedException {
 
         Scanner scanner = new Scanner(System.in);
+        int val = 1;
 
-        System.out.println("Meny:");
-        System.out.println("1. Start");
-        System.out.println("2. Avsluta");
-        System.out.println("3. Regler");
+        while (val != 2) {
 
+            System.out.println("Meny:");
+            System.out.println("1. Start");
+            System.out.println("2. Avsluta");
+            System.out.println("3. Regler");
 
-        int val = scanner.nextInt();
+            val = scanner.nextInt();
+            scanner.nextLine(); // Läs in det återstående radbytet
 
-        //Nedan måste vara inne i någon form av loop så att man alltid kan starta och avsluta.
- 
-        switch (val) {
-            case 1:
-                System.out.println("Du valde att starta spelet");
-                //här kan jag lägga till kod som ska användas för alternativ 1.
-                break;
-            case 2: 
-                System.out.println("Du valde att avsluta spelet.");
-                break;
-            case 3:
-                System.out.println("Du valde att se på reglerna.\n REGLER: \n 1. Att ha kul!");
-                break; 
-            default:
-                System.out.println("Ogiltigt svar. Var vänlig och välja mellan 1 av alternativen.");
+            switch (val) {
+                case 1:
+                    System.out.println("Du valde att starta spelet");
+                    System.out.println("Namnge Spelare: ");
+                    String spelar_namn = scanner.nextLine();
+                    System.out.println("Spelare: " + spelar_namn);
 
-        if starta spelet .... nedan: 
+                    int spelare_hp = 100;
+                    int dator_hp = 100;
 
-            System.out.println("första"); 
+                    Random random = new Random();
 
+                    while (spelare_hp > 0 && dator_hp > 0) {
+                        int spelare_attack = random.nextInt(20) + 1;
+                        int dator_attack = random.nextInt(20) + 1;
 
+                        dator_hp -= spelare_attack;
+                        spelare_hp -= dator_attack;
 
+                        
 
-            Random random = new Random();
+                        System.out.println("Spelarens hälsa: " + spelare_hp);
+                        System.out.println("Datorns hälsa: " + dator_hp);
+                        System.out.println("\n Nästa runda \n");
 
-            int player1 = random.nextInt(10);
-            int player2 = random.nextInt(10);
-            
-
-
-
-             
-
-                
+                        if (spelare_hp <= 0) {
+                            System.out.println("Du är nockad! Spelet är över.");
+                        } else if (dator_hp <= 0) {
+                            System.out.println("Datorn är nockad! Du vann!");
+                        }
+                        Thread.sleep(6000);
+                    }
+                    break;
+                case 2:
+                    System.out.println("Du valde att avsluta spelet.");
+                    scanner.close();
+                    break;
+                case 3:
+                    // Regelutskrift här
+                    break;
+                default:
+                    System.out.println("Ogiltigt svar. Var vänlig och välja mellan 1 av alternativen.");
+            }
         }
-
-
-
-
     }
-
-
 }
